7eb84a3dc1d19b7fec5136a88300183faebe6a81
Japar <abduljapar84@gmail.com>
2026-09-05 16:39:09 +0700

    Add base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abduljapar2025.github.io',
+  base: '/portofolio',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
