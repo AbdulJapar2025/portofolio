c8e898c0bb893dd9ec548130b66cea569b39bb03
Abdul Japar <abduljapar84@gmail.com>
2026-09-08 14:56:02 +0700

    Update base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abduljapar2025.github.io',
-  base: '/portofolio',
+  site: "https://abduljapar2025.github.io",
+  base: "/",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
